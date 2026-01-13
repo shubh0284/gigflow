33378f1420b3e78eeb3688270b8c18629d6cbaea
shubhamshinare <shubhamshinare2004@gmail.com>
2026-01-13 19:56:24 +0530

    Update production backend URLs

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://gigflow-q5i8.onrender.com/api", {
   withCredentials: true,
   autoConnect: true,
 });
